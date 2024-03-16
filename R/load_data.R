9ce30e1d573a108f3b31c0676ad5ec3db01a832b
santiagocel <u2292213@live.warwick.ac.uk>
2024-03-16 16:36:12 +0000

    added R and database file

--- R/load_data.R
+++ R/load_data.R
@@ -3,7 +3,7 @@ library(RSQLite)
 
 e.commerce_data <- readr::read_csv("ecommerce_data/ads_data.csv")
 my_connection <-RSQLite::dbConnect(RSQLite::SQLite(),"database/database.db")
-RSQLite::dbWriteTable(my_connection,"ads_data",e.commerce_data)
+RSQLite::dbWriteTable(my_connection,"ads_data",e.commerce_data, append=TRUE)
 
 
 
