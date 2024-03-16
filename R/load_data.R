471657c0f81a5cb319ac2a9c241eec3d7beb8655
santiagocel <u2292213@live.warwick.ac.uk>
2024-03-16 16:09:30 +0000

    added R file

--- R/load_data.R
+++ R/load_data.R
@@ -1,8 +1,8 @@
 library(readr)
 library(RSQLite)
 
-e.commerce_data <- readr::read_csv("/cloud/project/ecommerce_data/ads_data.csv")
-my_connection <-RSQLite::dbConnect(RSQLite::SQLite(),"/cloud/project/database/database.db")
+e.commerce_data <- readr::read_csv("ecommerce_data/ads_data.csv")
+my_connection <-RSQLite::dbConnect(RSQLite::SQLite(),"database/database.db")
 RSQLite::dbWriteTable(my_connection,"ads_data",e.commerce_data)
 
 
