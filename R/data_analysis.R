453fea0782d12d9e7d96d7e95a3e7b42ea3e444e
santiagocel <u2292213@live.warwick.ac.uk>
2024-03-18 17:33:06 +0000

    update

--- R/data_analysis.R
+++ R/data_analysis.R
@@ -1,5 +1,32 @@
-
+library(DBI)
+library(dplyr)
+library(RSQLite)
+library(readr)
 library(ggplot2)
+
+# 1.Connect to the database
+my_connection <- dbConnect(SQLite(),"database/an_e_commerce.db")
+
+# Get list of CSV files in the ecommerce_data directory
+csv_files <- list.files("ecommerce_data", pattern = "\\.csv$", full.names = TRUE)
+
+# Loop through the list of files and load them into their respective tables
+for (csv_file in csv_files) {
+  table_name <- gsub("\\.csv", "", basename(csv_file))  # Extract table name from file name without extension
+  
+  # Load data into R environment
+  assign(table_name, read_csv(csv_file))
+  
+  # Load data into SQLite database
+  print(paste("Loading file:", csv_file))  # Debugging message
+  data <- read_csv(csv_file)
+  dbWriteTable(my_connection, table_name, data, append = TRUE)
+  print(paste("File", csv_file, "successfully loaded into table", table_name))  # Debugging message
+}
+
+# List tables in the database
+tables <- dbListTables(my_connection)
+print(paste("Tables in the database:", toString(tables)))  # Debugging message
 
 ads = ads_data
 category = category_data
@@ -53,3 +80,5 @@ ggplot(customer, aes(x = age1)) +
   scale_x_continuous(breaks = seq(0, max(customer$age1), by = 10)) +
   theme_minimal()
 
+# Disconnect
+dbDisconnect(my_connection)
